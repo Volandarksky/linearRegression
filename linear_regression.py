fa82dd643439fdee8baa364a2ce8e75e2dc288f4
kk <kk@localhost.localdomain>
2017-10-22 15:00:15 +0400

    fix var

--- linear_regression.py
+++ linear_regression.py
@@ -7,9 +7,9 @@ dataframe = pd.read_fwf('linear_regression_dataset.txt')
 x_values = dataframe[['x']]
 y_values = dataframe[['y']]
 
-body_reg = linear_model.LinearRegression()
-body_reg.fit(x_values, y_values)
+y_reg = linear_model.LinearRegression()
+y_reg.fit(x_values, y_values)
 
 plt.scatter(x_values, y_values)
-plt.plot(x_values, body_reg.predict(x_values))
+plt.plot(x_values, y_reg.predict(x_values))
 plt.show()
